0106e165d6f6e2939cfa418c7cdaefda41d5c4d0
PhilipH3ro <PhilipH3ro@gmail.com>
2022-03-10 20:35:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,311 @@
+// 1. DOM - komma åt element ?
+const sectionList = document.getElementById('sectionList')
+const sectionNew = document.getElementById('sectionNew')
+const sectionEdit = document.getElementById('sectionEdit')
+
+const submitNewButton = document.getElementById('submitNewButton')
+const submitEditButton = document.getElementById('submitEditButton')
+const listLink = document.getElementById('listLink')
+const newLink = document.getElementById('newLink')
+
+const newNamn =  document.getElementById('newNamn')
+const newJersey = document.getElementById('newJersey')
+const newAge = document.getElementById('newAge')
+const newBorn = document.getElementById('newBorn')
+
+const editNamn =  document.getElementById('editNamn')
+const editJersey = document.getElementById('editJersey')
+const editAge = document.getElementById('editAge')
+const editBorn = document.getElementById('editBorn')
+
+const productTableBody = document.getElementById('productTableBody')
+const sortingTest = document.getElementById('sortingTest')
+
+//Todo ändra stefan till er egen - dvs {useremail}
+const baseApi = 'https://hockeyplayers.systementor.se/Philip/player'
+//const baseApi = 'https://fakestoreapi.com/products'
+// HTTP GET  Lista alla https://fakestoreapi.com/products
+// HTTP GET  Lista en https://fakestoreapi.com/products/4
+
+// HTTP POST Skapa en ny https://fakestoreapi.com/products 
+//                          - Skicka in ny product som JSON
+
+// HTTP PUT Uppdatera en befintlig https://fakestoreapi.com/products/4 
+//                          - Skicka in nya properties som JSON
+
+
+//Vi fortsätter från 
+// https://github.com/aspcodenet/kyhinlamninggrund
+
+//1. OOP - class Product id, name, price, color
+//2. in i items och rendera
+//3. Vi fixar NEW
+//4. Vi fixar edit
+
+class User{
+    constructor(id,namn,jersey,age, born){
+        this.id = id;
+        this.namn = namn;
+        this.jersey = jersey;
+        this.age = age;
+        this.born = born;
+   }
+}
+
+
+// const prod = new Product(1,"Banan",12,"Gul")
+// console.log(prod)
+
+// prod.Stad = "Colorado"
+// console.log(prod)
+
+// let namn = "Stefan"
+
+
+// console.log({namn})
+// let id = 129
+// console.log({id})
+
+// console.log("Klar")
+
+
+
+
+
+
+function showSection(sectionsId){
+    if(sectionsId == 'sectionList'){
+        sectionList.style.display = "block";
+        sectionNew.style.display = "none";
+        sectionEdit.style.display = "none";
+    }
+    else if(sectionsId == 'sectionNew'){
+        sectionList.style.display = "none";
+        sectionNew.style.display = "block";
+        sectionEdit.style.display = "none";
+    }
+    else if(sectionsId == 'sectionEdit'){
+        sectionList.style.display = "none";
+        sectionNew.style.display = "none";
+        sectionEdit.style.display = "block";
+    }
+}
+
+newLink.addEventListener("click",()=>{ 
+        showSection('sectionNew');    
+  });
+
+listLink.addEventListener("click",()=>{ 
+    showSection('sectionList');    
+});
+
+// 1. finns inte alls nån max. Finns dock en som heter Stefan som är förvirrad ;)
+// 2. Edit 
+//
+sortingTest.addEventListener("click",()=>{ 
+
+    items.sort(function (a, b) {
+        return b.id - a.id;
+      });
+      
+    console.log(items.sort());
+    refreshItems();
+    showSection('sectionList');
+
+    //console.log("test");
+});
+
+const searchId = document.getElementById('searchId')
+searchId.addEventListener("keyup", ()=>{
+    let filterList =  items.filter(item=> item.id.toString()
+        .includes( searchId.value ) );
+    userTableBody.innerHTML = '';
+    filterList.forEach( (item) => {
+        renderTr(item);
+    });
+})
+
+const searchUser = document.getElementById('searchUser')
+searchUser.addEventListener("keyup", ()=>{
+    let filterList =  items.filter(item=> item.namn.toLowerCase()
+        .includes( searchUser.value.toLowerCase() ) );
+    userTableBody.innerHTML = '';
+    filterList.forEach( (item) => {
+        renderTr(item);
+    });
+})
+
+const searchJersey = document.getElementById('searchJersey')
+searchJersey.addEventListener("keyup", ()=>{
+    let filterList =  items.filter(item=> item.jersey.toString()
+        .includes( searchJersey.value ) );
+    userTableBody.innerHTML = '';
+    filterList.forEach( (item) => {
+        renderTr(item);
+    });
+})
+
+const searchAge = document.getElementById('searchAge')
+searchAge.addEventListener("keyup", ()=>{
+    let filterList =  items.filter(item=> item.age.toString()
+        .includes( searchAge.value ) );
+    userTableBody.innerHTML = '';
+    filterList.forEach( (item) => {
+        renderTr(item);
+    });
+})
+
+
+const searchBorn = document.getElementById('searchBorn')
+searchBorn.addEventListener("keyup", ()=>{
+    let filterList =  items.filter(item=> item.born.toLowerCase()
+        .includes( searchBorn.value.toLowerCase() ) );
+    userTableBody.innerHTML = '';
+    filterList.forEach( (item) => {
+        renderTr(item);
+    });
+})
+
+submitNewButton.addEventListener("click",()=>{ 
+
+    const newUser = {
+        namn: newNamn.value,
+        jersey: newJersey.value,
+        age: newAge.value,
+        born: newBorn.value,
+    };
+
+    const reqParams = {
+        method:"POST",
+        headers:{
+            "content-type": "application/json"
+        },
+        body:JSON.stringify(newUser)
+    };
+    fetch(baseApi,reqParams)
+        .then(res=>res.json())
+        .then(json=>{
+            const u = new User(
+                json.id,
+                newNamn.value,
+                newJersey.value,
+                newAge.value,
+                newBorn.value)
+
+            items.push(u); 
+            renderTr(u);
+            showSection('sectionList');    
+        })
+});
+
+submitEditButton.addEventListener("click",()=>{
+
+    const changedUserValues = {
+        namn: editNamn.value,
+        jersey: editJersey.value,
+        age: editAge.value,
+        born: editBorn.value
+    };
+    const reqParams = {
+        method:"PUT",
+        headers:{
+            "content-type": "application/json"
+        },
+        body:JSON.stringify(changedUserValues)
+    };
+
+    // 'https://fakestoreapi.com/products/7
+
+
+    fetch(baseApi + '/' + editingUser.id ,reqParams)
+        .then(res=>{
+            //console.log(res);
+
+            refreshItems();
+            showSection('sectionList');   
+        });
+
+});
+
+let editingUser = null;
+
+function editUser(id){
+    editingUser = items.find((item)=>item.id == id)
+    editNamn.value = editingUser.namn;
+    editJersey.value = editingUser.jersey;
+    editAge.value = editingUser.age;
+    editBorn.value = editingUser.born;
+    showSection('sectionEdit');   
+}
+
+
+
+
+function renderTr(user){
+    let jsCall = `editUser(${user.id})`;
+    // jsCall = editProduct(18)
+    // jsCall = editProduct(19)
+    let template = `<tr>
+                        <td>${user.id}</td>
+                        <td>${user.namn}</td>
+                        <td>${user.jersey}</td>
+                        <td>${user.age}</td>
+                        <td>${user.born}</td>
+                        <td><a href="#" onclick="${jsCall}">EDIT</a></td>
+                    </tr>`
+    userTableBody.innerHTML = userTableBody.innerHTML + template;
+} 
+// 
+
+function refreshItems(){
+
+    // fetch('https://hockeyplayers.systementor.se/stefan/player')
+    // .then(response=>response.json())
+    // .then(array=>{
+    //     //json -> items
+    //     console.log(array)
+    // });
+
+    items = [];
+    userTableBody.innerHTML = '';
+
+    fetch(baseApi)
+        .then(response=>response.json())
+        .then(array=>{
+            //json -> items
+            console.log(array)
+            array.forEach(user=>{
+                u = new User(user.id,
+                    user.namn,
+                    user.jersey,
+                    user.age,
+                    user.born)                    
+                items.push(u);
+            });
+            items.forEach( (item) => {
+                renderTr(item);
+            });
+    })
+
+}
+
+let items = [];
+refreshItems();
+
+
+//Loopa den
+// för varje skapa tr, för varje skapa td:s 
+//lägga in den nya tr:n som ett barn till  productTableBody
+
+  
+
+showSection('sectionList');
+
+// 2. funktion showSection
+// 3. events = händelsestyrd programmering
+//      vid klick på länk -> showSection
+// 4. arrayer med strängar
+// 5. foreach!
+// 6. classes - Product 
+//      samt new:a in i array
+// 7. 
